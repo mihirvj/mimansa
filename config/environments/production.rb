fa40b4aff30ec41abe01c5875dddc75a1f5bec90
Fenil Kavathia <fenil.kavathia28@gmail.com>
2013-09-30 23:37:17 -0400

    ata tari ho chayla!!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Mimansa::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
   #config.assets.precompile += %w( highcharts.js )
-  #config.assets.precompile += %w( rails.js )
+  config.assets.precompile += %w( rails.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
